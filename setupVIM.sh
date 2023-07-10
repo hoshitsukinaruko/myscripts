deb2f63538d1da90192ac7aa388354c2d755086c
星月なるこ <34113239+naruko-hstk@users.noreply.github.com>
2023-07-10 09:04:05 +0000

    Add nvim support

--- setupVIM.sh
+++ setupVIM.sh
@@ -48,6 +48,7 @@ prompt -i "Setting up vim..."
 
 if command -v nvim; then
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  mkdir -p ~/.config/nvim
   curl https://raw.githubusercontent.com/naruko-hstk/myscripts/master/vim/init.vim --output ~/.config/nvim/init.vim --silent
   curl https://raw.githubusercontent.com/naruko-hstk/myscripts/master/vim/coc-config.vim --output ~/.config/nvim/coc-config.vim --silent
   curl https://raw.githubusercontent.com/naruko-hstk/myscripts/master/vim/plugin.vim --output ~/.config/nvim/plugin.vim --silent
@@ -55,6 +56,7 @@ if command -v nvim; then
   git clone https://github.com/doki-theme/doki-theme-vim.git ~/.local/share/nvim/site/pack/themes/start
 else
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  mkdir -p ~/.vim
   curl https://raw.githubusercontent.com/naruko-hstk/myscripts/master/vim/.vimrc --output ~/.vimrc --silent
   curl https://raw.githubusercontent.com/naruko-hstk/myscripts/master/vim/coc-config.vim --output ~/.vim/coc-config.vim --silent
   curl https://raw.githubusercontent.com/naruko-hstk/myscripts/master/vim/plugin.vim --output ~/.vim/plugin.vim --silent
